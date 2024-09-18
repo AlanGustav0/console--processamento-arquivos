5ba26948dcb37d35d964044fe8ff24f81efc3d0b
AlanGustav0 <alagustavo946@gmail.com>
2024-09-18 08:25:24 -0300

    refactor: Altered variables name and implements FileProcess class.

---
+++ ByteBankIO/FileProcess.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace ByteBankIO
+{
+    public class FileProcess
+    {
+        public static void DoWork()
+        {
+            var filePath = "contas.txt";
+            var currentBytes = -1;
+            var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+            var buffer = new byte[1024]; // 1KB
+            var sw = new Stopwatch();
+
+            sw.Start();
+            while (currentBytes != 0)
+            {
+                currentBytes = fileStream.Read(buffer, 0, 1024);
+                WriteBuffer(buffer, currentBytes);
+            }
+            sw.Stop();
+
+            Console.WriteLine($"Memória utilizada: {Process.GetCurrentProcess().WorkingSet64 / 1024 / 1024} mb");
+            Console.WriteLine($"Tempo de processamento: {sw.ElapsedMilliseconds} ms");
+            Console.ReadLine();
+        }
+
+        private static void WriteBuffer(byte[] buffer, int bytesLidos)
+        {
+            var utf8 = new UTF8Encoding();
+
+            var texto = utf8.GetString(buffer, 0, bytesLidos);
+            Console.Write(texto);
+        }
+    }
+}
--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -1,32 +1,10 @@
 ﻿using ByteBankIO;
-using System.Diagnostics;
-using System.Text;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var enderecoDoArquivo = "contas.txt";
-        var numeroDeBytesLidos = -1;
-        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open, FileAccess.Read);
-
-        var buffer = new byte[1024]; // 1KB
-
-        while (numeroDeBytesLidos != 0)
-        {
-            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-            EscreverBuffer(buffer, numeroDeBytesLidos);
-        }
-
-        Console.WriteLine($"\nMemória utilizada: {Process.GetCurrentProcess().WorkingSet64 / 1024 / 1024} mb");
-        Console.ReadLine();
+        FileProcess.DoWork();
     }
 
-    static void EscreverBuffer(byte[] buffer, int bytesLidos)
-    {
-        var utf8 = new UTF8Encoding();
-
-        var texto = utf8.GetString(buffer, 0, bytesLidos);
-        Console.Write(texto);
-    }
 }
